b57ef6a418ec668481f39451f7dfa9187560426f
sarahromanes <sarah.romanes@hotmail.com>
2018-03-22 21:12:48 +1100

    test

---
+++ R/power.R
@@ -0,0 +1,12 @@
+#' the multiDA function
+#'
+#' Takes in any numeric value and square it
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export
+
+multiDA<-function(x){
+  return(x^2)
+}
+
+
