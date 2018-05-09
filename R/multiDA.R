3355a0f91326fde2ad2018e9ab52d1f9a7615260
Sarah Romanes <you@example.com>
2018-05-09 14:31:06 +1000

    edit bugs

--- R/multiDA.R
+++ R/multiDA.R
@@ -134,7 +134,7 @@ multiDA <- function(mX,vy, c.pen=2,
   inds <- which(apply(res$mGamma,1,which.max)!=1) #non null cases
    
  
-  est.gamma <- apply(as.matrix(res$mGamma[inds,],1,max))
+  est.gamma <- apply(as.matrix(res$mGamma[inds,]),1,max)
 
   mR <- data.frame("rank"=rank(-est.gamma),"feature ID" = colnames(mX)[inds],"gamma.hat"=est.gamma,"partition"=apply(res$mGamma,1,which.max)[inds])
   mR <- mR[order(mR$rank),]
