09584df142783e2afb518435141ed146a3e93bba
Sarah Romanes <you@example.com>
2018-06-21 15:23:44 +1000

    updated vignette :penguin:

--- inst/doc/multiDAvignette.R
+++ inst/doc/multiDAvignette.R
@@ -8,14 +8,25 @@ res  <- multiDA(mX=mX, vy=vy, penalty="EBIC", equal.var=TRUE, set.options="exhau
 ## ------------------------------------------------------------------------
 vals <- predict(res, newdata=mX)$vy.pred          
 rser <- sum(vals!=vy)/length(vy)
+rser
 
 ## ------------------------------------------------------------------------
 print(res)
 
 ## ------------------------------------------------------------------------
-p <- plot(res, ranks = 1)
+plot(res, ranks = 1)
 
 ## ------------------------------------------------------------------------
-p1 <- plot(res, ranked=FALSE, features = c("V22", "V122"))
-p1
+plot(res, ranked=FALSE, features = c("V22", "V122"))
+
+## ------------------------------------------------------------------------
+glimpse_multiDA(res)
+
+## ------------------------------------------------------------------------
+tidy_res <- tidy_multiDA(res)
+head(tidy_res)
+
+## ------------------------------------------------------------------------
+augment_res <- augment_multiDA(res)
+dim(augment_res) #twice as long!
 
